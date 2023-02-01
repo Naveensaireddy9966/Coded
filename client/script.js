0a9dd0650361d7bdbd8f53c813ca5d0dc2496ca5
naveen <naveensaireddy123@gmail.com>
2023-02-01 15:56:03 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://coded-3uto.onrender.com/', {
+    const response = await fetch('https://coded-3uto.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
